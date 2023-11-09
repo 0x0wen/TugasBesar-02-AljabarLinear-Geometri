e2aeef96f87cfc78cdbfff046c32b6960786568d
Axel Santadi <axelsantadi@gmail.com>
2023-11-09 13:26:00 +0700

    benerin tekstur (sekarang hopefully bener), plus udh lumayan optimal, 1 gambar sekita 0,7 detik

--- loadgambartekstur.py
+++ loadgambartekstur.py
@@ -1,24 +1,27 @@
 import cv2
 import os
 import tekstur
+import time
 
 # Membaca gambar dari file
-gambar1 = cv2.imread('hololive-suisei.jpg')
+path1 = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\TugasBesar-Algeo-2\\0.jpg"
+gambar1 = cv2.imread(path1)
 totcon1,tothom1,totent1 = tekstur.CBIR_Tekstur(gambar1)
 
-path = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\buat_tes"
+path = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\TugasBesar-Algeo-2\\dataset"
 daftar_file = os.listdir(path)
 
-# Mengakses fitur warna dari setiap piksel
+#Mengakses fitur warna dari setiap piksel
 i = 2
 for nama_file in daftar_file:
+    start = time.time()
     gambar2 = cv2.imread(os.path.join(path, nama_file))
     totcon2,tothom2,totent2 = tekstur.CBIR_Tekstur(gambar2)
 
-    sim1 = tekstur.similarity(totcon1,totcon2)
-    sim2 = tekstur.similarity(tothom1,tothom2)
-    sim3 = tekstur.similarity(totent1,totent2)
-    
-    cossim1 = ((sim1+sim2+sim3) / 3) * 100
-    print(f"Tingkat kecocokan gambar {1} & {i} = {cossim1:.2f}%") 
+    sim = tekstur.similarity(totcon1,tothom1, totent1, totcon2, tothom2, totent2) * 100
+
+    print(f"Tingkat kecocokan gambar {1} & {i} = {sim}") 
+    end = time.time()
+    selis = end - start
+    print("waktu = ",selis)
     i += 1
--- tekstur.py
+++ tekstur.py
@@ -1,93 +1,81 @@
 import cv2
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 def Normalisasi(r,g,b):
-    r *= 29/100
-    g *= 587/1000
-    b *= 114/1000
-    y = round(r+g+b)
+    r = r * 0.29
+    g = g * 0.587
+    b = b * 0.114
+    y = r+g+b
     return y
 
 def coocurenceMatrix(list, tinggi, lebar):
-    cooc = np.zeros((256,256))
+    cooc = np.zeros((256,256), dtype=int)
 
     for i in range(tinggi):
         for j in range(lebar-1):
-            X = list[i, j]
-            Y = list[i, j+1]
+            X = round(list[i,j])
+            Y = round(list[i,j+1])
             cooc[X,Y] += 1
 
     return cooc
 
 def symmetricMatrix(matrix):
-    tMatrix = np.zeros((256,256))
-
-    for i in range(255):
-        for j in range(255):
-            tMatrix[i,j] = matrix[j,i]
-
-    symMatrix = np.zeros((256,256))
-    for i in range(255):
-        for j in range(255):
-            symMatrix[i,j] = matrix[i,j]+tMatrix[i,j]
+    symMatrix = np.zeros((256,256), dtype=float)
+    
+    symMatrix = matrix + np.transpose(matrix)
     
     return symMatrix
 
-def contrast(matrix):
-    totcon = 0
-    for i in range(255):
-        for j in range(255):
-            totcon += matrix[i,j] * ((i-j) ** 2)
+def normalizationMatrix(matrix):
+    pembagi = np.sum(matrix)
     
+    norm = np.zeros((256,256),dtype=float)
+    norm = matrix / pembagi
+
+    return norm
+
+def contrast(matrix):
+    totcon = np.sum(matrix * (np.arange(matrix.shape[0])[:, None] - np.arange(matrix.shape[1]))**2)
+
     return totcon
 
 def homogenity(matrix):
-    tothom = 0
-
-    for i in range(255):
-        for j in range(255):
-            tothom += matrix[i,j] / (1 + ((i - j) ** 2))
-    
+    tothom = np.sum(matrix / (1 + np.abs(np.arange(matrix.shape[0])[:, None] - np.arange(matrix.shape[1]))))
     return tothom
 
 def entropy(matrix):
-    totent = 0
-
-    for i in range(255):
-        for j in range(255):
-            totent += -1*(matrix[i,j]*np.log(matrix[i,j]))
+    epsilon = 1e-8
+    matrix = np.maximum(matrix, epsilon)
+    totent = -np.sum(matrix * np.log2(matrix))
     
     return totent
 
-def CBIR_Tekstur(gambar):
-    tinggi = gambar.shape[0]
-    lebar = gambar.shape[1]
+def CBIR_Tekstur(gambar1):
+    tinggi = gambar1.shape[0]
+    lebar = gambar1.shape[1]
     list_Y = np.zeros((tinggi,lebar))
+    r, g, b = cv2.split(gambar1)
 
-    for i in range(tinggi):
-        for j in range(lebar):
-            r = gambar[i,j,2]
-            g = gambar[i,j,1]
-            b = gambar[i,j,0]
-            Y = Normalisasi(r,g,b)
-            list_Y[i,j] = Y
+    list_Y = Normalisasi(r,g,b)
 
-    totcon = contrast(list_Y) 
-    tothom = homogenity(list_Y)
-    totent = entropy(list_Y)
+    cooc = coocurenceMatrix(list_Y, tinggi, lebar)
+    coocsim = symmetricMatrix(cooc)
+    norm = normalizationMatrix(coocsim)
+
+    totcon = contrast(norm)
+    tothom = homogenity(norm)
+    totent = entropy(norm)
 
     return totcon,tothom,totent
 
-def similarity(list1,list2):
-    sum = 0
-    panjangvek1 = 0;
-    panjangvek2 = 0;
+def similarity(totcon1, tothom1, totent1, totcon2, tothom2, totent2):
+    vektor_1 = np.array([totcon1, tothom1, totent1])
+    vektor_2 = np.array([totcon2, tothom2, totent2])
 
-    sum += (list1 * list2)
-    panjangvek1 += list1**2
-    panjangvek2 += list2**2
-    
-    panjangvek1 **= (1/2)  
-    panjangvek2 **= (1/2)
-    
-    return sum / (panjangvek1 * panjangvek2) 
+    vektor_1 = vektor_1 / np.linalg.norm(vektor_1)
+    vektor_2 = vektor_2 / np.linalg.norm(vektor_2)
+
+    similarity = cosine_similarity([vektor_1], [vektor_2])[0][0]
+
+    return similarity
