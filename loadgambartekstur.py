56e342ed4e003ef9ec2722b3fa2f4df74c15341b
Axel Santadi <axelsantadi@gmail.com>
2023-11-11 19:53:18 +0700

    update baru (0.15an detik per foto)

--- loadgambartekstur.py
+++ loadgambartekstur.py
@@ -2,26 +2,46 @@ import cv2
 import os
 import tekstur
 import time
+from skimage.feature import graycomatrix, graycoprops
+import numpy as np
 
-# Membaca gambar dari file
-path1 = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\TugasBesar-Algeo-2\\0.jpg"
+'''# Membaca gambar dari file
 gambar1 = cv2.imread(path1)
 totcon1,tothom1,totent1 = tekstur.CBIR_Tekstur(gambar1)
 
-path = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\TugasBesar-Algeo-2\\dataset"
-daftar_file = os.listdir(path)
 
 #Mengakses fitur warna dari setiap piksel
 i = 2
-for nama_file in daftar_file:
     start = time.time()
     gambar2 = cv2.imread(os.path.join(path, nama_file))
     totcon2,tothom2,totent2 = tekstur.CBIR_Tekstur(gambar2)
 
-    sim = tekstur.similarity(totcon1,tothom1, totent1, totcon2, tothom2, totent2) * 100
+    sim = tekstur.similarity(totcon1,tothom1, totent1, totcon2, tothom2, totent2)
 
     print(f"Tingkat kecocokan gambar {1} & {i} = {sim}") 
     end = time.time()
     selis = end - start
+    print("waktu = ",selis)
+    i += 1'''
+
+path1 = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\TugasBesar-Algeo-2\\0.jpg"
+image = cv2.imread(path1)
+reference_image = tekstur.grayscale_image(image)
+reference_features = tekstur.calculate_texture_features(reference_image)
+
+# Loop untuk membandingkan dengan ribuan gambar lainnya
+path = "C:\\Users\\Axel Santadi\\Documents\\Cool_Yeah\\Tingkat_2\\Semester_3\\AlGeo\\Tubes2\\TugasBesar-Algeo-2\\dataset"
+daftar_file = os.listdir(path)
+i = 2
+for nama_file in daftar_file:
+    start = time.time()
+    compared_image = cv2.imread(os.path.join(path, nama_file))
+    compared_image = tekstur.grayscale_image(compared_image)
+    compared_features = tekstur.calculate_texture_features(compared_image)
+
+    similarity = tekstur.cosine_similarity(reference_features, compared_features)
+    end = time.time()
+    selis = end - start
+    print(f'Similarity with image_{i}.jpg: {similarity:.4f}')
     print("waktu = ",selis)
     i += 1
--- tekstur.py
+++ tekstur.py
@@ -1,4 +1,4 @@
-import cv2
+'''import cv2
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -7,7 +7,8 @@ def Normalisasi(r,g,b):
     g = g * 0.587
     b = b * 0.114
     y = r+g+b
-    return y
+    yInt = y.astype(int)
+    return yInt
 
 def coocurenceMatrix(list, tinggi, lebar):
     cooc = np.zeros((256,256), dtype=int)
@@ -78,4 +79,35 @@ def similarity(totcon1, tothom1, totent1, totcon2, tothom2, totent2):
 
     similarity = cosine_similarity([vektor_1], [vektor_2])[0][0]
 
-    return similarity
+    return similarity'''
+
+import cv2
+import numpy as np
+from scipy.spatial import distance
+
+def grayscale_image(image):
+    r, g, b = cv2.split(image)
+    r = r * 0.29
+    g = g * 0.587
+    b = b * 0.114
+    y = r+g+b
+    yInt = y.astype(int)
+    return yInt
+
+def calculate_texture_features(image):
+    co_occurrence_matrix = np.zeros((256, 256), dtype=int)
+    height, width = image.shape
+
+    for i in range(height):
+        for j in range(width):
+            if i < height - 1 and j < width - 1:
+                co_occurrence_matrix[image[i, j], image[i+1, j+1]] += 1
+
+    contrast = np.sum(np.square(np.arange(256)[:, np.newaxis] - np.arange(256)[np.newaxis, :]) * co_occurrence_matrix)
+    entropy = -np.sum(co_occurrence_matrix * np.log(co_occurrence_matrix + np.finfo(float).eps))
+    homogeneity = np.sum(co_occurrence_matrix / (1 + np.abs(np.arange(256)[:, np.newaxis] - np.arange(256)[np.newaxis, :])))
+
+    return contrast, entropy, homogeneity
+
+def cosine_similarity(vector1, vector2):
+    return 1 - distance.cosine(vector1, vector2)    
