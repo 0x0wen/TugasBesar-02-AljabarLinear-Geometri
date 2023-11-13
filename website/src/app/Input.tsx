5d79a90cc9d3a84937e9a1e2feecdc9f03c2d607
heisenberg <owentobias21@gmail.com>
2023-11-13 10:34:24 +0700

    update input

--- website/src/app/Input.tsx
+++ website/src/app/Input.tsx
@@ -1,28 +1,49 @@
 'use client'
 import Image from 'next/image'
 import Button from '@mui/material/Button'
-import {styled} from '@mui/material/styles'
 import Stack from '@mui/material/Stack'
-import {Switch} from '@mui/material'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import SearchIcon from '@mui/icons-material/Search'
 import FileUploadIcon from '@mui/icons-material/FileUpload'
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
 import CollectionsIcon from '@mui/icons-material/Collections'
 import VisuallyHiddenInput from './components/input/VisuallyHiddenInput'
-import color from '../../public/assets/color.png'
-import texture from '../../public/assets/texture.png'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Slider from '@mui/material/Slider'
 import MaterialUISwitch from './components/buttons/MaterialUISwitch'
-import {useState} from 'react'
+import {useState, useRef, useEffect} from 'react'
 import Camera from './Camera'
-const Input = () => {
+const Input = ({
+	onSearch,
+}: {
+	onSearch: (data: {
+		parameter: string
+		threshold: number
+		file: File | null
+	}) => void
+}) => {
 	const [selectedFile, setSelectedFile] = useState(null)
-	const [value, setValue] = useState<number>(60)
 	const [showCamera, setShowCamera] = useState(false)
+	const [threshold, setThreshold] = useState(60)
+	const [checked, setChecked] = useState(true)
+	const formRef = useRef(null)
 
+	// Use useEffect to submit the form after each photo is taken
+	useEffect(() => {
+		let intervalId: any
+
+		if (showCamera) {
+			// Enable automatic form submission only when the camera is open
+			intervalId = setInterval(() => {
+				formRef.current && formRef.current.submit()
+			}, 5000)
+		}
+		return () => clearInterval(intervalId)
+	}, [showCamera])
+	const checkHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setChecked(event.target.checked)
+	}
 	const onOpenCamera = () => {
 		setShowCamera(true)
 	}
@@ -33,20 +54,29 @@ const Input = () => {
 		const file = event.target.files[0]
 		setSelectedFile(file)
 	}
-	const handleChange = (event: Event, newValue: number | number[]) => {
+	const thresholdHandler = (event: Event, newValue: number | number[]) => {
 		if (typeof newValue === 'number') {
-			setValue(newValue)
+			setThreshold(newValue)
 		}
 	}
-	const onSearch = () =>{
-
+	const handleSearch = (e: any) => {
+		e.preventDefault()
+		onSearch({parameter, threshold, selectedFile})
+	}
+	const handleSearchCamera = (image: any) => {
+		setSelectedFile(image)
 	}
 	const imageURL = selectedFile ? URL.createObjectURL(selectedFile) : null
+	const parameter = checked ? 'texture' : 'color'
 	return (
 		/* upload image section */
-		<section className="upload text-center w-fit mx-auto flex flex-wrap justify-center  gap-4 md:gap-8 lg:gap-20 m-4 p-4 rounded-xl text-color1 shadow-md shadow-color2">
+		<form
+			ref={formRef}
+			onSubmit={handleSearch}
+			className="upload text-center w-fit mx-auto flex flex-wrap justify-center  gap-4 md:gap-8 lg:gap-20 m-4 p-4 rounded-xl text-color1 shadow-md shadow-color2"
+		>
 			{showCamera ? (
-				<Camera />
+				<Camera onSearch={handleSearchCamera} />
 			) : (
 				<div className="relative border-2 border-white aspect-[4/3] w-80 sm:w-96 md:w-[28rem] lg:w-[32rem] h-auto flex justify-center items-center">
 					<Button
@@ -81,7 +111,7 @@ const Input = () => {
 					<section className="flex gap-4">
 						<Button
 							component="label"
-							variant="contained"
+							variant={showCamera ? 'outlined' : 'contained'}
 							className="w-fit mx-auto"
 							startIcon={<FileUploadIcon />}
 							onClick={onCloseCamera}
@@ -95,7 +125,7 @@ const Input = () => {
 						</Button>
 						<Button
 							component="label"
-							variant="contained"
+							variant={showCamera ? 'contained' : 'outlined'}
 							className="w-fit mx-auto"
 							startIcon={<PhotoCameraIcon />}
 							onClick={onOpenCamera}
@@ -113,7 +143,9 @@ const Input = () => {
 					>
 						<Typography>Color</Typography>
 						<FormControlLabel
-							control={<MaterialUISwitch defaultChecked />}
+							control={
+								<MaterialUISwitch checked={checked} onChange={checkHandler} />
+							}
 							label=""
 						/>
 						<Typography>Texture</Typography>
@@ -121,31 +153,28 @@ const Input = () => {
 
 					<Box sx={{width: 250}}>
 						<Typography id="non-linear-slider" gutterBottom>
-							Threshold: {'>'}
-							{value}%
+							Threshold: {threshold}%
 						</Typography>
 						<Slider
-							value={value}
-							min={5}
+							value={threshold}
+							min={10}
 							step={1}
 							max={100}
-							onChange={handleChange}
-							valueLabelDisplay="auto"
+							onChange={thresholdHandler}
 							aria-labelledby="non-linear-slider"
 						/>
 					</Box>
 					<Button
-						component="label"
-						variant="contained"
+						variant="outlined"
 						className="w-fit mx-auto"
 						endIcon={<SearchIcon />}
-						onSubmit={onSearch}
+						type="submit"
 					>
 						Search
 					</Button>
 				</section>
 			</section>
-		</section>
+		</form>
 	)
 }
 
