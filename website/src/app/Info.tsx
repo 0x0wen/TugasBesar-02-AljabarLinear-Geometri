0bf9cb0d9ed3c559d20937f6adde3d0c1d5cc4f5
heisenberg <owentobias21@gmail.com>
2023-11-15 20:24:44 +0700

    Update Info.tsx

--- website/src/app/Info.tsx
+++ website/src/app/Info.tsx
@@ -4,45 +4,58 @@ import HelpIcon from '@mui/icons-material/Help'
 import CancelIcon from '@mui/icons-material/Cancel'
 import {useState, useEffect} from 'react'
 import RevealStagger from './components/animations/RevealStagger'
+import RevealWithBG from './components/animations/RevealWithBG'
+import Divider from '@mui/material/Divider'
 import Link from 'next/link'
 const InfoModal = ({style}: {style: string}) => {
 	return (
 		<div
-			className={`" bg-color3 text-center py-20 fixed bottom-0 right-0 flex flex-col gap-10 transition-all duration-500 overflow-y-auto " ${style}`}
+			className={`" bg-color1 text-color5 text-center py-20 fixed bottom-0 right-0 flex flex-col gap-10 transition-all z-20 duration-500 overflow-y-auto " ${style}`}
 		>
-			<RevealStagger custom={5}>
-				<section>
-					<h1 className="font-anton text-color1 text-4xl mb-4">
-						What is this shit?
+			<section className="text-center flex flex-wrap justify-center">
+				<RevealWithBG>
+					<h1 className="font-anton text-4xl mb-4 drop-shadow-2xl ">
+						What is LENSA?
 					</h1>
-					<p className="font-montserrat text-color1 px-5 text-justify">
-						Content-Based Image Retrieval (CBIR) is an advanced search technique
-						that allows you to find images not by traditional text-based tags,
-						but by their visual content. It's like searching with your eyes!
-						CBIR technology assesses images based on their texture and color
-						patterns, making it a powerful tool for visual search.
+				</RevealWithBG>
+				<RevealWithBG>
+					<p className="font-montserrat  px-5 sm:px-10 md:px-14 lg:px-64 text-justify">
+						LENSA is a implementation of{' '}
+						<strong>Content-Based Image Retrieval</strong> (CBIR), which is an
+						advanced search technique that allows you to find images not by
+						traditional text-based tags, but by their visual content. It's like
+						searching with your eyes! CBIR technology assesses images based on
+						their texture and color patterns, making it a powerful tool for
+						visual search.
 					</p>
-				</section>
-			</RevealStagger>
-			<RevealStagger custom={6}>
-				<section>
-					<h1 className="font-anton text-color1 text-4xl mb-4">How to use?</h1>
-					<p className="font-montserrat text-color1 px-5 text-justify">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-						magni blanditiis maiores culpa illum dignissimos ducimus alias aut
-						officiis consequatur dolorum minima ratione nemo, quisquam labore
-						autem facere tempore in placeat voluptas consequuntur. Quidem, aut
-						voluptatum. Architecto voluptates officiis esse!
-					</p>
-					<ol className="font-montserrat text-color1 px-5 text-justify">
-						<li>
-							<strong>Upload Your Dataset:</strong> Begin by uploading your
-							dataset of images to our system. This dataset will be used as a
-							reference for future searches.
+				</RevealWithBG>
+			</section>
+
+			<section className="text-center flex flex-wrap justify-center">
+				<RevealWithBG>
+					<h1 className="font-anton  text-4xl mb-4 shadow-white drop-shadow-2xl">
+						How to use?
+					</h1>
+				</RevealWithBG>
+				<ol className="font-montserrat  px-5 sm:px-10 md:px-14 lg:px-64 text-justify">
+					<RevealWithBG>
+						<li className="">
+							<p className="text-color5 font-bold pl-5 text-lg">
+								1. Upload Your Dataset
+							</p>
+							Begin by uploading your dataset of images to our system. This
+							dataset will be used as the source of the query search using CBIR.
+							You can use the same dataset more than 1 time without needing to
+							re-upload it again and again, but once you change the dataset or
+							refresh the page, the dataset will be overwritten.
 						</li>
+					</RevealWithBG>
+					<RevealWithBG>
 						<li>
-							<strong>Add Your Image:</strong> Next, you can add your query
-							image in two ways:
+							<p className="text-color5 font-bold pl-5 text-lg">
+								2. Add Your Image
+							</p>
+							Next, you can add your query image in two ways:
 							<ul className="ml-10">
 								<li>
 									<strong>Upload Image File:</strong> Upload an image file from
@@ -51,52 +64,67 @@ const InfoModal = ({style}: {style: string}) => {
 								<li>
 									<strong>Use Integrated Camera:</strong> Alternatively, you can
 									use the integrated camera on our website to capture an image
-									directly.
+									directly. The camera will automatically screenshot the image
+									every 5 seconds, and perform search immediately.
 								</li>
 							</ul>
 						</li>
+					</RevealWithBG>
+					<RevealWithBG>
 						<li>
-							<strong>Set Your Preferences:</strong> Customize your search by
-							setting your preferred parameters. These settings will influence the search results. There are 2 settings:
+							<p className="text-color5 font-bold pl-5 text-lg">
+								3. Set Your Preferences
+							</p>
+							Customize your search by setting your preferred parameters. These
+							settings will influence the search results. There are 2 settings:
 							<ul className="ml-10">
 								<li>
-									<strong>Parameter</strong> Upload an image file from
-									your device to use as a query.
+									<strong>Parameter:</strong> Search similar images based on the
+									'color' or 'texture' of the query image.
 								</li>
 								<li>
-									<strong>Threshold</strong> Alternatively, you can
-									use the integrated camera on our website to capture an image
-									directly.
+									<strong>Threshold:</strong> Diplay results above certain
+									threshold you desire.
 								</li>
 							</ul>
 						</li>
+					</RevealWithBG>
+					<RevealWithBG>
 						<li>
-							<strong>Click Search:</strong> Hit the 'Search' button to initiate
-							the query. Our system will process your request and identify
-							images that match your criteria.
+							<p className="text-color5 font-bold pl-5 text-lg">
+								4. Click Search
+							</p>
+							Hit the 'Search' button to initiate the query. Our system will
+							process your request and identify images that match your criteria.
 						</li>
+					</RevealWithBG>
+					<RevealWithBG>
 						<li>
-							<strong>View Results:</strong> A collection of images that surpass
+							<p className="text-color5 font-bold pl-5 text-lg">
+								5. View Results
+							</p>
+							A collection of images and it's similarity percentage that surpass
 							the similarity threshold will be presented to you. Explore the
 							results to find images that align with your query.
 						</li>
-					</ol>
-				</section>
-			</RevealStagger>
-			<RevealStagger custom={7}>
-				<section>
-					<h1 className="font-anton text-color1 text-4xl mb-4">
+					</RevealWithBG>
+				</ol>
+			</section>
+
+			<section className="text-center flex flex-wrap justify-center">
+				<RevealWithBG>
+					<h1 className="font-anton  text-4xl mb-4 drop-shadow-2xl">
 						About the creators
 					</h1>
-					<p className="font-montserrat text-color1 px-5 text-justify">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-						magni blanditiis maiores culpa illum dignissimos ducimus alias aut
-						officiis consequatur dolorum minima ratione nemo, quisquam labore
-						autem facere tempore in placeat voluptas consequuntur. Quidem, aut
-						voluptatum. Architecto voluptates officiis esse!
+				</RevealWithBG>
+				<RevealWithBG>
+					<p className="font-montserrat  px-5 sm:px-10 md:px-14 lg:px-64 text-justify">
+						We are just some curious student studying Informatics Engineering at
+						Bandung Institute of Technology. We made this website to fulfill the
+						Linear Algebra and Geometry project.
 					</p>
-				</section>
-			</RevealStagger>
+				</RevealWithBG>
+			</section>
 		</div>
 	)
 }
@@ -120,16 +148,18 @@ const Info = () => {
 	return (
 		<div>
 			<InfoModal style={styling} />
-			<div className="flex items-end fixed bottom-5 right-5 z-20 ">
+			<div className="flex items-end fixed bottom-5 right-5 z-20">
 				{modal ? (
 					<IconButton
 						aria-label="delete"
 						color="primary"
 						onClick={modalHandler}
+						className="aspect-square w-20 h-auto"
 					>
 						<CancelIcon
 							fontSize="inherit"
-							sx={{width: '50px', height: '50px', scale: '2:hover'}}
+							sx={{scale: '2:hover', color: '#1EFF96'}}
+							className="w-full h-full"
 						/>
 					</IconButton>
 				) : (
@@ -137,10 +167,15 @@ const Info = () => {
 						aria-label="delete"
 						color="primary"
 						onClick={modalHandler}
+						className="aspect-square w-20 h-auto"
 					>
 						<HelpIcon
 							fontSize="inherit"
-							sx={{width: '50px', height: '50px', scale: '2:hover'}}
+							sx={{
+								scale: '2:hover',
+								color: '#1EFF96',
+							}}
+							className="w-full h-full"
 						/>
 					</IconButton>
 				)}
