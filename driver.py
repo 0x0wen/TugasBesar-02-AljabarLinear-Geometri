366240d49041780e20e637963987a5cc40194ba3
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2023-11-17 17:55:06 +0700

    BENERIN return

--- driver.py
+++ driver.py
@@ -1,36 +1,26 @@
+import base64
 import test
-import os
+import numpy as np
 import cv2
-import csv
 
-def ubahArrayGambarJadiHistHSV(path_gambar,path_csv):
+def ubahArrayGambarJadiHistHSV(file_contents: bytes):
+    
+    nparr = np.frombuffer(file_contents, np.uint8)
+    ArrayGambar = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
     arrayobjects = []
-    ArrayGambar = os.listdir(path_gambar)
-    i = 1
     for image in ArrayGambar:
-        compared_image = cv2.imread(os.path.join(path_gambar, image))
-        image_resize = cv2.resize(compared_image, (0,0), fx = 0.5, fy = 0.5)
+        image_resize = cv2.resize(image, (0,0), fx = 0.5, fy = 0.5)
         H,S,V = test.histHSV(image_resize)
+        
+        _, buffer = cv2.imencode('.jpg', image)
+        img_encode = base64.b64encode(buffer.tobytes()).decode("utf-8")
+        
         arrayobjects.append({
-            "image": compared_image,
+            "image": img_encode,
             "hist_h": H,
             "hist_s": S,
             "hist_v": V,
         })
-        print(f'gambar {i} sudah jadi')
-        i+=1
     
-    with open(path_csv, 'w', newline='') as csv_file:
-        # Specify the field names (column headers)
-        fieldnames = ['image', 'hist_h', 'hist_s', 'hist_v']
-        
-        # Create a CSV writer object
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        # Write the header row
-        writer.writeheader()
-
-        # Write the data rows
-        for row in arrayobjects:
-            writer.writerow(row)
-    
+    return arrayobjects
---
+++ loadfix.py
@@ -0,0 +1,40 @@
+import cv2
+import test
+import time
+
+def mainColor(minim,img,arrayobject):
+    gambar_resized  = cv2.resize(img,(0,0),fx = 0.5 , fy = 0.5)
+    H1,S1,V1 = test.histHSV(gambar_resized)
+
+    sim = []
+    arrayImgdanSim = []
+    arrayakhir = []
+    
+    mulai = time.time()
+    for row in arrayobject:
+        H2,S2,V2 = row['hist_h'],row['hist_s'],row['hist_v']
+        similarity = (test.similarity(H1,H2) + test.similarity(S1,S2) + test.similarity(V1,V2)) / 3 * 100
+        if similarity >= minim:
+            if (not arrayImgdanSim):
+                arrayImgdanSim.append({
+                    "image" : row['image'],
+                    "persentasePersamaan" : similarity
+                })
+            else:
+                indeks = 0
+                while indeks < len(arrayImgdanSim) and (arrayImgdanSim[indeks])['persentaseSim'] > similarity:
+                    indeks += 1
+                masuk = ({
+                    "image" : row['image'],
+                    "persentasePersamaan" : similarity
+                })
+                arrayImgdanSim.insert(indeks, masuk)
+    selesai = time.time()
+    selisih = selesai - mulai
+    
+    arrayakhir.append({
+        "arrayImgdanSim" : arrayImgdanSim,
+        "waktuOperasi" : selisih 
+    })
+    
+    return arrayakhir
