5b927e4719172d54872d1c359e52fa0415ed1cc9
Axel Santadi <axelsantadi@gmail.com>
2023-11-17 19:10:23 +0700

    ikohnubgvjgymh
    jijhougytfvyuyikjlkn

--- prosestekstur.py
+++ prosestekstur.py
@@ -5,14 +5,15 @@ import csv
 import numpy as np
 import base64
 
-def ubahArrayGambarMenjadiListCooc(file_contents: bytes):
-    nparr = np.frombuffer(file_contents, np.uint8)
-    ArrayGambar = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+def ubahArrayGambarMenjadiListCooc(arrayFile):
     arrayobjects = []
-    for image in ArrayGambar:
-        image_resize = cv2.resize(image, (0,0), fx = 0.5, fy = 0.5)
+    for fileImage in arrayFile:
+        nparr = np.frombuffer(fileImage, np.uint8)
+        gambar = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        image_resize = cv2.resize(gambar, (0,0), fx = 0.5, fy = 0.5)
         contrast, entropy, homogeneity = tekstur.calculate_texture_features(image_resize)
-        _, buffer = cv2.imencode('.jpg', image)
+        _, buffer = cv2.imencode('.jpg', gambar)
         img_base64 = base64.b64encode(buffer).decode("utf-8")
         arrayobjects.append({
             "image": img_base64,
